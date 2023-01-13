cd7ce56e6b249525e279c48f20305bfe1e9a60ca
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-13 20:04:30 +0100

    get selected word fallback for win

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -1,3 +1,4 @@
+from time import sleep
 from typing import Callable
 from pynput import keyboard
 from controller.KeyListener import KeyListener
@@ -5,6 +6,7 @@ from controller.get_selected_word import get_selected_word
 from model.Command import Command
 from model.Context import Context
 from view.get_window import get_window
+import threading
 
 
 class Controller:
@@ -12,12 +14,29 @@ class Controller:
     def __init__(self, context: Context) -> None:
         self.context = context
         self.k_lstnr = KeyListener()
+        self.cmd_queue = []
 
         for c in context.config.combos:
-            self.k_lstnr.add_combo(c.keys, self.command_to_method(c.command))
+            self.k_lstnr.add_combo(
+                c.keys, lambda: self.add_to_cmd_queue(c.command))
 
     def start(self):
         self.k_lstnr.start()
+        threading.Thread(target=self.process_events, args=()).start()
+
+    def process_events(self):
+
+        while True:
+
+            for c in self.cmd_queue:
+                self.command_to_method(c)()
+
+            self.cmd_queue.clear()
+
+            sleep(0.2)
+
+    def add_to_cmd_queue(self, cmd: Command):
+        self.cmd_queue.append(cmd)
 
     def command_to_method(self, cmd: Command) -> Callable:
 
@@ -28,6 +47,10 @@ class Controller:
     def lookup_word(self):
 
         selected_word = get_selected_word(self.context.config)
+
+        if not selected_word:
+            return
+
         dict_entry = self.context.word_dict[selected_word]
 
         if dict_entry:
--- src/controller/get_selected_word.py
+++ src/controller/get_selected_word.py
@@ -1,7 +1,30 @@
 import os
-
+from time import sleep
+from typing import Optional
 from model.Config import Config
+from pynput.keyboard import Controller, Key, KeyCode
+import pyperclip
 
-def get_selected_word(config:Config):
-    return os.popen('xsel').read()
-    
+
+def get_selected_word(config: Config):
+    return linux() or fallback()
+
+def linux() -> Optional[str]:
+
+    try:
+        return os.popen('xsel').read()
+    except:
+        return None
+
+
+def fallback() -> Optional[str]:
+
+    c = Controller()
+    c.press(Key.ctrl)
+    c.press(KeyCode.from_char('c'))
+    sleep(0.2)
+    c.release(KeyCode.from_char('c'))
+    c.release(Key.ctrl)
+
+    curr_word = pyperclip.paste()
+    return curr_word
