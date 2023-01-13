5b8f8a1f4772b43a729d9b0f8c5f397fad2fafc7
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-13 01:35:06 +0100

    ...

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,9 @@
-
 from model.Context import Context
 from controller.Controller import Controller
 
-context  = Context.get_context()
-
+context = Context.get_context()
 controller = Controller(context)
-
 controller.start()
 
 while True:
     pass
-
-# print(context.word_dict['dog'])
