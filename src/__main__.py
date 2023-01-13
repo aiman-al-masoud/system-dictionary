5f740788b1a604a3cef8d5e0b198a9c91d2c3296
aiman-al-masoud <luxlunarislabs@gmail.com>
2023-01-13 21:56:51 +0100

    ...

--- src/__main__.py
+++ src/__main__.py
@@ -3,8 +3,6 @@ from model.Context import Context
 from controller.Controller import Controller
 
 context = Context.get_context()
-
-# print(context.config)
 controller = Controller(context)
 controller.start()
 
